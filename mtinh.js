ab691f4cfaf7cfa583da4cc8cdf3edf8c5f4346a
duongnhat0801 <nhatx0801@gmail.com>
2024-03-12 10:27:20 +0700

    a

---
+++ mtinh.js
@@ -0,0 +1,16 @@
+let view = document.getElementById('view');
+function inputValue(){
+    view.value += value;
+}
+function result() {
+    let result
+    ()
+    {
+        let result = eval(view.value);
+        view.value = result;
+    }
+
+    function D(){
+        view.value = "";
+    }
+}
